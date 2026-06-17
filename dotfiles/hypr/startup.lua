25ff482bec6e70ce580365a47108ea037e90c796
mindset <mindset@Mindset>
2026-06-17 18:18:32 +0700

    fix(hypr): improve dbus env sync for portal stability

--- dotfiles/hypr/startup.lua
+++ dotfiles/hypr/startup.lua
@@ -4,8 +4,7 @@
 
 hl.on("hyprland.start", function()
     -- Import env for systemd
-    hl.exec_cmd(
-    "systemctl --user import-environment DISPLAY WAYLAND_DISPLAY XDG_CURRENT_DESKTOP XDG_SESSION_DESKTOP XDG_SESSION_TYPE")
+    hl.exec_cmd("dbus-update-activation-environment --systemd --all")
 
     -- Clipboard history
     hl.exec_cmd("wl-paste --watch cliphist store")
